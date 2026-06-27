3b98d12395dca5ad35f173ba95183b54111f2400
Prithisharavi <prithishaselvakumar@gmail.com>
2026-06-27 15:46:28 +0530

    Added Exercise 5

---
+++ WEEK-1/JUnit, Mockito and SL4J/Mockito/Exercise_5_Multiple_Returns/MyService.java
@@ -0,0 +1,15 @@
+package com.cognizant.mockito.exercise5;
+
+public class MyService {
+
+    private ExternalApi externalApi;
+
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public String fetchData() {
+        return externalApi.getData();
+    }
+
+}
