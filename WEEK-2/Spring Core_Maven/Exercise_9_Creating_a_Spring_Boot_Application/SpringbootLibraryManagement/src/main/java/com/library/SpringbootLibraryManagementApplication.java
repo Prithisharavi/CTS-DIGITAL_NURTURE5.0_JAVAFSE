4738b455d09971a834a7c1e7734a00e3672845e3
Prithisharavi <prithishaselvakumar@gmail.com>
2026-06-29 14:08:52 +0530

    Added Exercise 9

---
+++ WEEK-2/Spring Core_Maven/Exercise_9_Creating_a_Spring_Boot_Application/SpringbootLibraryManagement/src/main/java/com/library/SpringbootLibraryManagementApplication.java
@@ -0,0 +1,12 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLibraryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLibraryManagementApplication.class, args);
+    }
+}
