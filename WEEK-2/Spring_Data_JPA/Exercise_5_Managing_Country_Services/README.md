3e5bdd91b8d560742ed48b0e2ad1749348ed5425
Prithisharavi <prithishaselvakumar@gmail.com>
2026-06-30 10:25:43 +0530

    Added Exercise 5

---
+++ WEEK-2/Spring_Data_JPA/Exercise_5_Managing_Country_Services/README.md
@@ -0,0 +1,22 @@
+# Exercise 5 - Managing Country Services
+
+## Objective
+Implement CRUD operations using Spring Data JPA.
+
+## Features Implemented
+- Get all countries
+- Get country by code
+- Add a new country
+- Update country details
+- Delete a country
+- Search countries by partial name
+
+## Technologies Used
+- Spring Boot
+- Spring Data JPA
+- Hibernate
+- MySQL
+- Maven
+
+## Output
+Successfully executed all country service operations.
---
+++ WEEK-2/Spring_Data_JPA/Exercise_5_Managing_Country_Services/orm-learn/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,47 @@
+package com.cognizant.ormlearn.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional(readOnly = true)
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Country getCountry(String code) {
+        return countryRepository.findByCode(code);
+    }
+
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+
+    @Transactional
+    public void updateCountry(Country country) {
+        countryRepository.save(country);
+    }
+
+    @Transactional
+    public void deleteCountry(String code) {
+        countryRepository.deleteById(code);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Country> searchCountry(String name) {
+        return countryRepository.findByNameContainingIgnoreCase(name);
+    }
+}
